671cab59b14f626fc262e73553fd56432413b555
Ilya Urbanovich <urbilya@gmail.com>
2020-09-22 15:23:20 +0300

    Add cl_run.c
    This file contains implementation of run_cl function, which
    manages cl buffers, runs cl kernel and puts mlx image in it's
    current state to the screen.

---
+++ src/cl_run.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cl_run.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nalysann <urbilya@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/18 18:22:57 by nalysann          #+#    #+#             */
+/*   Updated: 2020/09/18 18:22:58 by nalysann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef __APPLE__
+# include <OpenCL/opencl.h>
+#else
+# include <CL/cl.h>
+#endif
+
+#include "cl_data.h"
+#include "error.h"
+#include "fractol.h"
+
+#include "ft_error.h"
+#include "mlx.h"
+
+void	run_cl(t_fractol *fractol)
+{
+	t_cl			*cl;
+	const size_t	global_work_size = fractol->size_x * fractol->size_y;
+
+	cl = &fractol->cl;
+	cl->status = clEnqueueWriteBuffer(cl->queue, cl->data, CL_TRUE, 0,
+								sizeof(t_draw), &fractol->data, 0, NULL, NULL);
+	if (cl->status != CL_SUCCESS)
+	{
+		ft_throw(BUFFER_W_MSG, E_OPENCL);
+	}
+	cl->status = clEnqueueNDRangeKernel(cl->queue, cl->kernel, 1, NULL,
+									&global_work_size, NULL, 0, NULL, NULL);
+	if (cl->status != CL_SUCCESS)
+	{
+		ft_throw(EXECUTE_MSG, E_OPENCL);
+	}
+	cl->status = clEnqueueReadBuffer(cl->queue, cl->img, CL_TRUE, 0,
+								sizeof(int) * fractol->size_x * fractol->size_y,
+								fractol->img.data_addr, 0, NULL, NULL);
+	if (cl->status != CL_SUCCESS)
+	{
+		ft_throw(BUFFER_R_MSG, E_OPENCL);
+	}
+	mlx_put_image_to_window(fractol->mlx_ptr, fractol->win_ptr,
+							fractol->img.ptr, 0, 0);
+}
