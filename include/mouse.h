31aacd301a52ab68233c9682228c863b511a53f6
Ilya Urbanovich <urbilya@gmail.com>
2020-09-22 14:22:53 +0300

    Add mouse.h
    This file contains defines for mouse buttons and prototypes
    for hook and specific button handler functions.

---
+++ include/mouse.h
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mouse.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nalysann <urbilya@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/14 12:42:46 by nalysann          #+#    #+#             */
+/*   Updated: 2020/09/14 12:42:46 by nalysann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MOUSE_H
+# define MOUSE_H
+
+# include "fractol.h"
+
+/*
+** Button codes
+*/
+
+# define BUT_LEFT		1
+# define BUT_RIGHT		2
+# define BUT_MIDDLE		3
+# define BUT_UP			4
+# define BUT_DOWN		5
+
+/*
+** Hook functions
+*/
+
+int		button_press(int button, int x, int y, void *param);
+int		button_release(int button, int x, int y, void *param);
+int		motion_notify(int x, int y, void *param);
+
+/*
+** Button handlers
+*/
+
+void	zoom(int button, int x, int y, t_fractol *fractol);
+
+#endif
