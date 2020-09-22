b69d000061f949d8cc7869be89cfa755c0bfbc03
Ilya Urbanovich <urbilya@gmail.com>
2020-09-22 14:12:54 +0300

    Add cl_funs.h
    This header file contains functions used for initializing t_cl structure
    and calling the kernel.

---
+++ include/cl_funs.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cl_funs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nalysann <urbilya@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/19 15:09:53 by nalysann          #+#    #+#             */
+/*   Updated: 2020/09/19 15:09:56 by nalysann         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CL_FUNS_H
+# define CL_FUNS_H
+
+# include "fractol.h"
+
+void	init_cl(t_fractol *fractol);
+void	run_cl(t_fractol *fractol);
+
+#endif
