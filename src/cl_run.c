08cf1be899b54263c5d961f9dd760c6ec3962ec0
nalysann <urbilya@gmail.com>
2020-10-09 13:53:53 +0300

    Update cl_run.c to use the latest libft version

--- src/cl_run.c
+++ src/cl_run.c
@@ -17,40 +17,39 @@
 #endif
 
 #include "cl_data.h"
+#include "draw.h"
 #include "error.h"
 #include "fractol.h"
 
-#include "ft_error.h"
+#include "ft_stdio.h"
+
 #include "mlx.h"
 
 void	run_cl(t_fractol *fractol)
 {
 	t_cl			*cl;
-	const size_t	global_work_size = fractol->int_params[1] *
-										fractol->int_params[2];
+	const size_t	global_work_size = fractol->sizex * fractol->sizey;
 
 	cl = &fractol->cl;
-	cl->status = clEnqueueWriteBuffer(cl->queue, cl->int_params, CL_TRUE, 0,
-				sizeof(int) * INT_PARAMS, fractol->int_params, 0, NULL, NULL);
+	cl->status = clEnqueueWriteBuffer(cl->queue, cl->data, CL_TRUE, 0,
+				sizeof(t_draw), &fractol->data, 0, NULL, NULL);
 	if (cl->status != CL_SUCCESS)
-		ft_throw(BUFFER_W_MSG, E_OPENCL);
-	cl->status = clEnqueueWriteBuffer(cl->queue, cl->double_params, CL_TRUE, 0,
-		sizeof(double) * double_PARAMS, fractol->double_params, 0, NULL, NULL);
-	if (cl->status != CL_SUCCESS)
-		ft_throw(BUFFER_W_MSG, E_OPENCL);
-	cl->status = clEnqueueWriteBuffer(cl->queue, cl->palette, CL_TRUE, 0,
-sizeof(int) * (fractol->int_params[3] + 1), fractol->palette, 0, NULL, NULL);
-	if (cl->status != CL_SUCCESS)
-		ft_throw(BUFFER_W_MSG, E_OPENCL);
+	{
+		exit_with_error(BUFFER_W_MSG, E_OPENCL);
+	}
 	cl->status = clEnqueueNDRangeKernel(cl->queue, cl->kernel, 1, NULL,
 									&global_work_size, NULL, 0, NULL, NULL);
 	if (cl->status != CL_SUCCESS)
-		ft_throw(EXECUTE_MSG, E_OPENCL);
+	{
+		exit_with_error(EXECUTE_MSG, E_OPENCL);
+	}
 	cl->status = clEnqueueReadBuffer(cl->queue, cl->img, CL_TRUE, 0,
-				sizeof(int) * fractol->int_params[1] * fractol->int_params[2],
+				sizeof(int) * fractol->data.size_line_int * fractol->sizey,
 				fractol->img.data_addr, 0, NULL, NULL);
 	if (cl->status != CL_SUCCESS)
-		ft_throw(BUFFER_R_MSG, E_OPENCL);
+	{
+		exit_with_error(BUFFER_R_MSG, E_OPENCL);
+	}
 	mlx_put_image_to_window(fractol->mlx_ptr, fractol->win_ptr,
 							fractol->img.ptr, 0, 0);
 }
