8a7d078e60a5d1dc36e85ed15b362867e800e450
nalysann <urbilya@gmail.com>
2020-10-09 13:52:44 +0300

    Update cl_init.c to use the latest libft version

--- src/cl_init.c
+++ src/cl_init.c
@@ -17,27 +17,28 @@
 #endif
 
 #include "cl_data.h"
+#include "draw.h"
 #include "error.h"
 #include "fractol.h"
 #include "utils.h"
 
-#include "ft_error.h"
+#include "ft_stdio.h"
 
 static void		get_platform(t_cl *cl)
 {
 	cl->status = clGetPlatformIDs(0, NULL, &cl->num_platforms);
 	if (cl->status != CL_SUCCESS)
 	{
-		ft_throw(PLAT_MSG, E_OPENCL);
+		exit_with_error(PLAT_MSG, E_OPENCL);
 	}
 	if (cl->num_platforms != PLATFORMS)
 	{
-		ft_throw(N_PLAT_MSG, E_OPENCL);
+		exit_with_error(N_PLAT_MSG, E_OPENCL);
 	}
 	cl->status = clGetPlatformIDs(PLATFORMS, &cl->platform, NULL);
 	if (cl->status != CL_SUCCESS)
 	{
-		ft_throw(G_PLAT_MSG, E_OPENCL);
+		exit_with_error(G_PLAT_MSG, E_OPENCL);
 	}
 }
 
@@ -54,17 +55,17 @@ static void		get_device(t_cl *cl)
 	}
 	if (cl->status != CL_SUCCESS)
 	{
-		ft_throw(DEV_MSG, E_OPENCL);
+		exit_with_error(DEV_MSG, E_OPENCL);
 	}
 	if (cl->num_devices != DEVICES)
 	{
-		ft_throw(N_DEV_MSG, E_OPENCL);
+		exit_with_error(N_DEV_MSG, E_OPENCL);
 	}
 	cl->status = clGetDeviceIDs(cl->platform, cl->device_type, DEVICES,
 								&cl->device, NULL);
 	if (cl->status != CL_SUCCESS)
 	{
-		ft_throw(G_DEV_MSG, E_OPENCL);
+		exit_with_error(G_DEV_MSG, E_OPENCL);
 	}
 }
 
@@ -73,36 +74,30 @@ static void		init_kernel_args(t_fractol *fractol)
 	t_cl	*cl;
 
 	cl = &fractol->cl;
-	cl->int_params = clCreateBuffer(cl->context, CL_MEM_READ_ONLY,
-						sizeof(int) * INT_PARAMS, NULL, &cl->status);
+	cl->data = clCreateBuffer(cl->context, CL_MEM_READ_ONLY,
+						sizeof(t_draw), NULL, &cl->status);
 	if (cl->status != CL_SUCCESS)
-		ft_throw(BUFFER_MSG, E_OPENCL);
-	cl->double_params = clCreateBuffer(cl->context, CL_MEM_READ_ONLY,
-				sizeof(double) * double_PARAMS, NULL, &cl->status);
-	if (cl->status != CL_SUCCESS)
-		ft_throw(BUFFER_MSG, E_OPENCL);
+	{
+		exit_with_error(BUFFER_MSG, E_OPENCL);
+	}
 	cl->img = clCreateBuffer(cl->context, CL_MEM_WRITE_ONLY, sizeof(int) *
-			fractol->int_params[1] * fractol->int_params[2], NULL, &cl->status);
+			fractol->data.size_line_int * fractol->sizey, NULL, &cl->status);
 	if (cl->status != CL_SUCCESS)
-		ft_throw(BUFFER_MSG, E_OPENCL);
-	cl->palette = clCreateBuffer(cl->context, CL_MEM_READ_ONLY, sizeof(int) *
-							(fractol->int_params[3] + 1), NULL, &cl->status);
+	{
+		exit_with_error(BUFFER_MSG, E_OPENCL);
+	}
+	cl->status = clSetKernelArg(cl->kernel, 0, sizeof(cl_mem), &cl->data);
 	if (cl->status != CL_SUCCESS)
-		ft_throw(BUFFER_MSG, E_OPENCL);
-	cl->status = clSetKernelArg(cl->kernel, 0, sizeof(cl_mem), &cl->int_params);
+	{
+		exit_with_error(ARG_MSG, E_OPENCL);
+	}
+	cl->status = clSetKernelArg(cl->kernel, 1, sizeof(cl_mem), &cl->img);
 	if (cl->status != CL_SUCCESS)
-		ft_throw(ARG_MSG, E_OPENCL);
-	cl->status = clSetKernelArg(cl->kernel, 1, sizeof(cl_mem), &cl->double_params);
-	if (cl->status != CL_SUCCESS)
-		ft_throw(ARG_MSG, E_OPENCL);
-	cl->status = clSetKernelArg(cl->kernel, 2, sizeof(cl_mem), &cl->img);
-	if (cl->status != CL_SUCCESS)
-		ft_throw(ARG_MSG, E_OPENCL);
-	cl->status = clSetKernelArg(cl->kernel, 3, sizeof(cl_mem), &cl->palette);
-	if (cl->status != CL_SUCCESS)
-		ft_throw(ARG_MSG, E_OPENCL);
+	{
+		exit_with_error(ARG_MSG, E_OPENCL);
+	}
 }
-#include <stdio.h>
+
 static void		init_kernel(t_fractol *fractol)
 {
 	t_cl	*cl;
@@ -114,26 +109,19 @@ static void		init_kernel(t_fractol *fractol)
 								(const char **)&kernel_str, NULL, &cl->status);
 	if (cl->status != CL_SUCCESS)
 	{
-		ft_throw(PROGRAM_MSG, E_OPENCL);
+		exit_with_error(PROGRAM_MSG, E_OPENCL);
 	}
 	free(kernel_str);
 	cl->status = clBuildProgram(cl->program, cl->num_devices, &cl->device,
 								NULL, NULL, NULL);
-	if (cl->status == CL_BUILD_PROGRAM_FAILURE) {
-		size_t log_size;
-		clGetProgramBuildInfo(cl->program, cl->device, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
-		char *log = (char *)malloc(log_size);
-		clGetProgramBuildInfo(cl->program, cl->device, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
-		printf("%s\n", log);
-	}
 	if (cl->status != CL_SUCCESS)
 	{
-		ft_throw(BUILD_MSG, E_OPENCL);
+		exit_with_error(BUILD_MSG, E_OPENCL);
 	}
 	cl->kernel = clCreateKernel(cl->program, "fractol", &cl->status);
 	if (cl->status != CL_SUCCESS)
 	{
-		ft_throw(KERNEL_MSG, E_OPENCL);
+		exit_with_error(KERNEL_MSG, E_OPENCL);
 	}
 	init_kernel_args(fractol);
 }
@@ -149,12 +137,12 @@ void			init_cl(t_fractol *fractol)
 									&cl->status);
 	if (cl->status != CL_SUCCESS)
 	{
-		ft_throw(CONTEXT_MSG, E_OPENCL);
+		exit_with_error(CONTEXT_MSG, E_OPENCL);
 	}
 	cl->queue = clCreateCommandQueue(cl->context, cl->device, 0, &cl->status);
 	if (cl->status != CL_SUCCESS)
 	{
-		ft_throw(QUEUE_MSG, E_OPENCL);
+		exit_with_error(QUEUE_MSG, E_OPENCL);
 	}
 	init_kernel(fractol);
 }
